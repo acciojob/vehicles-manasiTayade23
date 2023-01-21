e7fb86b1c881a333f3f65ce9db5670092c728fc6
Manasi <mansitayade23@gmail.com>
2023-01-21 16:11:32 +0530

    bug fixed

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -17,14 +17,14 @@ public class Vehicle {
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-         this.currentDirection =this.getCurrentDirection()+direction;
+         this.currentDirection +=direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        this.setCurrentDirection(speed);
-        this.setCurrentDirection(direction);
+       this.currentSpeed=speed;
+       this.currentDirection=direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
