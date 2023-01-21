fd272fee2cef6ef54ce466a468b77722caeed19c
Manasi <mansitayade23@gmail.com>
2023-01-21 15:52:18 +0530

    Answer 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
+        mercedes.move(10, 45);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
     }
